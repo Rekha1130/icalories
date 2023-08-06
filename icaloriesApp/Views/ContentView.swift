ca8050f015134b470b1a802d538d451037c79912
Rekha1130 <erina.rekha@gmail.com>
2023-08-06 14:27:31 -0700

    - Add EditFoodView file. - In ContentView add toolbar items, code for functions totalCaloriesToday() and deleteFood()

--- icaloriesApp/Views/ContentView.swift
+++ icaloriesApp/Views/ContentView.swift
@@ -26,14 +26,14 @@ struct ContentView: View {
                 
                 List {
                     ForEach(food) { food in
-                        NavigationLink(destination: Text("\(food.calories)")) {
+                        NavigationLink(destination: EditFoodView(food: food)) {
                             HStack {
                                 VStack(alignment: .leading, spacing: 6) {
                                     Text(food.name!)
                                         .bold()
                                     
                                     Text("\(Int(food.calories))") +
-                                    Text("calories").foregroundColor(.red)
+                                    Text(" calories").foregroundColor(.red)
                                 }
                                 Spacer()
                                 Text(calcTimeSince(date: food.date!))
@@ -48,17 +48,43 @@ struct ContentView: View {
                 .listStyle(.plain)
             }
             .navigationTitle("iCalories")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        showingAddView.toggle()
+                    } label: {
+                        Label("Add Food", systemImage: "plus.circle")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+            }
+            .sheet(isPresented: $showingAddView) {
+                AddFoodView()
+            }
         }
+        .navigationViewStyle(.stack)
     }
     
     private func totalCaloriesToday() -> Double {
         
-        return 0.0
+        var caloriesToday: Double = 0
+        for item in food {
+            if Calendar.current.isDateInToday(item.date!) {
+                caloriesToday += item.calories
+            }
+        }
+        return caloriesToday
     }
     
     private func deleteFood(offSets: IndexSet) {
         
-        
+        withAnimation {
+            offSets.map { food[$0] } .forEach(managedObjContext.delete)
+                
+            DataController().save(context: managedObjContext)
+        }
     }
 }
 
